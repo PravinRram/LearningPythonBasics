b7c76fd8173ce5ca668b8956dddb597df5ea7bae
Pravin <144076247+PravinRram@users.noreply.github.com>
2024-01-13 22:47:29 +0800

    Add files via upload

---
+++ Variables/Variables-Ex1.py
@@ -0,0 +1,11 @@
+name = 'John Smith'
+age = 20
+existingPatient = False
+
+'''
+Shown in vid:
+
+full-name = 'John Smith'
+age = 20
+is_New = True
+'''
---
+++ Variables/Variables.py
@@ -0,0 +1,9 @@
+price = 10
+price = 20
+print(price)
+'''code is read from top to bottom'''
+
+price = 10 #int
+rating = 4.9 #float
+name = 'theQWERTYkeyboard' #str
+isPublished = False #bool (has to be capitalised)
